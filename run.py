38352f3c2c5adfae513dbb77f2a9803b4916ec55
MariaHdezG <mariachg50@gmail.com>
2023-05-29 19:51:45 +0000

    initial commit

--- run.py
+++ run.py
@@ -12,3 +12,23 @@ def print_board(board):
     print('  A B C D E F G H')
     print(' *****************')
 
+ row_num=1
+    for row in board:
+        print("%d|%s|" % (row_num, "|".join(row)))
+        row_num +=1
+
+
+def get_ship_location():
+    #Enter the row number between 1 to 8
+    row=input('Please enter a ship row 1-8 ').upper()
+    while row not in '12345678':
+        print("Please enter a valid row ")
+        row=input('Please enter a ship row 1-8 ')
+         #Enter the Ship column from A TO H
+    column=input('Please enter a ship column A-H ').upper()
+    while column not in 'ABCDEFGH':
+        print("Please enter a valid column ")
+        column=input('Please enter a ship column A-H ')
+    return int(row)-1,let_to_num[column]
+
+

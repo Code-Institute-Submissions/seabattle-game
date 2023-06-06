ef1795e40fe31474a20171425bec2f8371172474
MariaHdezG <125393563+MariaHdezG@users.noreply.github.com>
2023-06-06 13:46:50 +0200

    Update run.py
    Add changes so the main codes have uppercase letters

--- run.py
+++ run.py
@@ -3,8 +3,8 @@
 # ‘ - ’ indicates the hits missed
 from random import randint
 
-Hidden_Pattern = [[' '] * 8 for x in range(8)]
-Guess_Pattern = [[' '] * 8 for x in range(8)]
+HIDDEN_PATTERN = [[' '] * 8 for x in range(8)]
+GUESS_PATTERN = [[' '] * 8 for x in range(8)]
 
 let_to_num = {'A':0,'B':1, 'C':2,'D':3,'E':4,'F':5,'G':6,'H':7}
 
